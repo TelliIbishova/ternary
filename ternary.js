eb140871e3029654a162ff3a01bb24ebe257cae1
TelliIbishova <telliibishova6@gmail.com>
2026-05-19 04:58:36 +0400

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,13 @@
+// ternary operator Tapşırığı — Endirim Hesabı
+// Məhsul qiyməti:
+// JavaScript
+// let price = 120;
+// Əgər qiymət 100 və ya daha çoxdursa:
+// 20% endirim tətbiq et.
+// Əks halda:
+// qiyməti dəyişmə.
+
+let price = prompt ("qiymeti daxil et");
+price >=100
+? alert ( price- (price * 20) / 100)
+: alert(price)
